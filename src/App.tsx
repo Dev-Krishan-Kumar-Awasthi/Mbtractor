9d1a2e7b458ae09d72711b0bae2dc02ca45f473c
Krishan Kumar Awasthi <awasthikrishnaa92@gmail.com>
2026-05-22 11:34:28 +0530

    Update to latest Lenis package to fix blank screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { ReactLenis } from '@studio-freight/react-lenis';
+import { ReactLenis } from 'lenis/react';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import WhatsAppButton from './components/WhatsAppButton';
